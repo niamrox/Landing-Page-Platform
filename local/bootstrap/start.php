5181c11a27fd7115b68a96eae0e14ed9f5b01f01
sem-nowsquare <s.kokhuis@nowsquare.com>
2017-02-14 21:53:58 +0100

    Updated PC name at Laravel environment settings (local/bootstrap/start.php)

--- local/bootstrap/start.php
+++ local/bootstrap/start.php
@@ -27,7 +27,7 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local' => array(
-        'XPS',
+        'PC-NAME',
     ),
 
 	'production' => array('*'),
